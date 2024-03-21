90c1f6dc76de9f7df38bb965997f4c68aa915e6d
Conner Lusk <lusk.c@northeastern.edu>
2024-03-21 11:14:16 -0400

    initialize

---
+++ nvim/lua/conner/plugins/nvim-tree.lua
@@ -0,0 +1,55 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local nvimtree = require("nvim-tree")
+
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		vim.o.termguicolors = false
+
+		-- change color for arrows in tree to light blue
+		vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+		vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+
+		-- configure nvim-tree
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeOpen<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		keymap.set("n", "<leader>r", "<cmd>NvimTreeClose<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+	end,
+}
